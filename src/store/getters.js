890c220a2d27435ad5a6481b37c56e8cc7af2c63
ToluwanimiO <toluwanimiosuolale@yahoo.co.uk>
2022-02-08 13:44:40 +0100

    commented out getters section

--- src/store/getters.js
+++ src/store/getters.js
@@ -1,9 +1,9 @@
-var _ = require('lodash');
-export const getters = {
-  FILTER_SPESIFIC_DATA: (state) =>{
-    return _.filter(state.data , (data) =>{
-      // ....
-      // console.log(data)
-    });
-  }
-};
+// var _ = require('lodash');
+// export const getters = {
+//   FILTER_SPESIFIC_DATA: (state) =>{
+//     return _.filter(state.data , (data) =>{
+//       // ....
+//       console.log(data)
+//     });
+//   }
+// };
