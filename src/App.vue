2e48f5e44e7d13ae7c31471e4f8ceb66e89b4ab6
ToluwanimiO <toluwanimiosuolale@yahoo.co.uk>
2022-02-08 01:07:56 +0100

    loading

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,36 @@
 <template>
+
   <div >
-    <NavBar/>
-    <router-view></router-view>
+    <div v-if="loadStatus">
+      <NavBar/>
+      <router-view></router-view>
+    </div>
+    <Loading v-if="!loadStatus"/>
   </div>
 </template>
 
 <script>
 import NavBar from './components/NavBar.vue'
+import Loading from './components/Loading.vue'
 
 export default {
   name: 'App',
+  data(){
+    return{
+      loadStatus:false
+    }
+  },
   components: {
     NavBar,
+    Loading
+  },
+  created:function(){
+    window.addEventListener('load', this.loaded);
+  },
+  methods:{
+    loaded:function(){
+      this.loadStatus=true
+    }
   }
 }
 </script>
---
+++ src/components/Loading.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="body text-center" >
+      <div class="pencil">
+        <div class="pencil__ball-point"></div>
+        <div class="pencil__cap"></div>
+        <div class="pencil__cap-base"></div>
+        <div class="pencil__middle"></div>
+        <div class="pencil__eraser"></div>
+    </div>
+    <div class="line"></div>
+    <h2>Page Loading...Please Wait</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.body {
+ font-family: sans-serif;
+ display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ min-height: 100vh;
+ background: #010001;
+ color: white;
+ font-size: 1.5rem;
+}
+.pencil {
+ position: relative;
+ width: 300px;
+ height: 40px;
+ transform-origin: center;
+ transform: rotate(135deg);
+ animation: pencil-animation 10s infinite;
+}
+@keyframes pencil-animation {
+ 0% {
+ transform: rotate(135deg);
+}
+ 20% {
+ transform: rotate(315deg);
+}
+ 45% {
+ transform: translateX(300px) rotate(315deg);
+}
+ 55% {
+ transform: translateX(300px) rotate(495deg);
+}
+ 100% {
+ transform: rotate(495deg);
+}
+}
+.pencil__ball-point {
+ position: absolute;
+ left: 0;
+ top: 50%;
+ transform: translateY(-50%);
+ background: #0A8BB2;
+ height: 10px;
+ width: 10px;
+ border-radius: 50px;
+}
+.pencil__cap {
+ position: absolute;
+ left: 0px;
+ top: 50%;
+ transform: translateY(-50%);
+ clip-path: polygon(20% 40%, 100% 0%, 100% 100%, 20% 60%);
+ background: #232123;
+ width: 12%;
+ height: 100%;
+}
+.pencil__cap-base {
+ position: absolute;
+ left: 12%;
+ top: 0;
+ height: 100%;
+ width: 20px;
+ background: #232123;
+}
+.pencil__middle {
+ position: absolute;
+ left: calc(12% + 20px);
+ top: 0;
+ height: 100%;
+ width: 70%;
+ background: #0A8BB2;
+}
+.pencil__eraser {
+ position: absolute;
+ left: calc(12% + 70% + 20px);
+ top: 0;
+ height: 100%;
+ width: 11%;
+ border-top-right-radius: 5px;
+ border-bottom-right-radius: 5px;
+ background: #232123;
+}
+.line {
+ position: relative;
+ top: 80px;
+ /* right: 103px; */
+ height: 10px;
+ width: 100%;
+ /* z-index: -1; */
+ border-radius: 50px;
+ background: #0A8BB2;
+ transform: scaleX(0);
+ transform-origin: center;
+ animation: line-animation 10s infinite;
+}
+@keyframes line-animation {
+ 20% {
+ transform: scaleX(0);
+}
+ 45% {
+ transform: scaleX(0.6);
+}
+ 55% {
+ transform: scaleX(0.6);
+}
+ 100% {
+ transform: scaleX(0);
+}
+}
+ h2 {
+	position: relative;
+	top: 150px;
+	/* right: 75px; */
+}
+ 
+</style>
